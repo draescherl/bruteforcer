e5c9dd64fe658eda6ad217086cbff97b94cda8ed
Lucas DRAESCHER <draescherl@cy-tech.fr>
2022-03-02 10:47:08 +0000

    Used to solve problem in class.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,7 @@ import ciphers.Caesar;
 public class Main {
 
     public static void main(String[] args) {
-        Bruteforcer bruteforcer = new Bruteforcer("PHHWEDQNHQDP", new Caesar());
-        bruteforcer.crack();
-        System.out.println("-----");
-        bruteforcer.setInput("XLIASVPHAMHIAIF");
-        bruteforcer.crack();
-        System.out.println("-----");
-        bruteforcer.setInput("YGOROTMOYCGEZUUYRUCZUZXGBKR");
+        Bruteforcer bruteforcer = new Bruteforcer("LTMTRJFSIXJJDTZSJCYBJJP", new Caesar());
         bruteforcer.crack();
     }
 
